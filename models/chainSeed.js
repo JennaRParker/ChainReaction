9176a4a45900a2ba3eae2787d2fac42cb405ac56
JENNA PARKER <github-actions[bot]@users.noreply.github.com>
2022-09-14 12:25:41 -0400

    add new chain page styled

--- models/chainSeed.js
+++ models/chainSeed.js
@@ -1,5 +1,18 @@
 module.exports = [{
     name: 'Starbucks',
     logo: 'https://logos-download.com/wp-content/uploads/2016/03/Starbucks_Logo_1987.png'
-},
+    },
+    {
+    name: 'Chik-Fil-A',
+    logo: 'http://media.nj.com/njo_top_stories/photo/chick-fil-a-e62e25bf8246c0b2.png'
+    },
+    {
+    name: 'QuikTrip',
+    logo: 'https://www.cstoredigitalranking.com/wp-content/uploads/2017/02/QuikTrip_logo-300x245.png'
+    },
+    {
+    name: 'Taco Bell',
+    logo: "http://1000logos.net/wp-content/uploads/2017/06/Taco-Bell-Logo.png"
+    }
+
 ]
