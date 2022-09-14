b9ae8342bd23cd1022a6bc5ffade64e1d4f2082d
JENNA PARKER <github-actions[bot]@users.noreply.github.com>
2022-09-14 11:34:39 -0400

    seed configured. styling

--- models/chains.js
+++ models/chains.js
@@ -1,27 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// const reactionSchema = new Schema(
-//     {
-//         name: {type: String, required: true },
-//         photo: String,
-//         comment: {type: String, required: true },
-//         rating: {type: Number, required: true },
-        
-//     },
-//     { timestamps: true }
-// );
 
 const chainSchema = new Schema(
     {
         name: { type: String, required: true },
         logo: { type: String, required: true },
-        reaction: {
-            name: {type : String },
-            photo: {type : String},
-            comment: {type: String},
-            rating: {type: Number}, 
-        }
     }
 );
 
